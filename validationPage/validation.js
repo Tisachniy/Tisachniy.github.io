0eac65a371cf19a47a4aea160ede130f5735188b
Nikita <itomatpast@gmail.com>
2019-08-07 13:31:30 +0200

    Added new alert

--- validationPage/validation.js
+++ validationPage/validation.js
@@ -3,11 +3,24 @@ function validate(){
     var login = document.getElementById("login").value;
     var password = document.getElementById("password").value;
     if(login == "devtest" || password == "devtest"){
-        alert("succes");
+        Swal.fire({
+            title: 'Correct data',
+            text: "enter tisachniy.github.io?",
+            type: 'success',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, go!'
+          }).then((result) => {
+            if (result.value) {
+                location.href = "/index/main.html";
+            }
+          });
+
         if(validationError){
             $("#validError").hide();
         }
-        location.href = "/index/main.html";
+        
     }else{
         var validationForm = document.getElementsByClassName("form-group")[0];
         $(validationForm).prepend("<h2 id='validError'>Invalid Login or Password</h2>");
