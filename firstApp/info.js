7dbad38e45acac65bc2824e0b12e2a61d46941ff
Nikita <itomatpast@gmail.com>
2019-10-09 22:01:49 +0300

    Trying fix shitty ajax

--- firstApp/info.js
+++ firstApp/info.js
@@ -1,7 +1,24 @@
 var ip;
-var access_key = '169083a424e366842cddbe3009ea001d';
-
+//var access_key = '96c8bea0-25b5-4ebd-a162-3d65cf6430e1';  
 $( document ).ready(function() {
+ 
+  
+  getIp();
+  addInfo();
+});
+
+// $.ajax({
+//   type: "GET",
+//   url: "https://ipfind.com/?ip="+ip+"&auth="+access_key,
+//   data: "data",
+//   dataType: "dataType",
+//   success: function (response) {
+//     console.log(response);
+//   }
+// });
+
+
+function getIp(){
   $(function() {
     $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
       function(json) {
@@ -10,34 +27,39 @@ $( document ).ready(function() {
       }
     );
   });
-    //addInfo();
-});
-
-
+}
 
 function addInfo(){
 var info = "";
-$.getJSON('https://ipapi.co/json/', function(data) {
-  console.log(JSON.stringify(data, null, 2));
-  info = data;
-  var ip = info.ip;
-  var country = info.country_name;
-  var city = info.city;
-  var timeZone = info.timezone;
-  var latitdue = info.latitude;
-  var longitude = info.longitude;
-  var provider = info.org;
-  var countrycode = info.country_calling_code;
-
-  //return info
-  $("#ip")[0].textContent = " " + ip;
-  $("#country")[0].textContent = " " + country;
-  $("#city")[0].textContent = " " + city;
-  $("#timezone")[0].textContent = " " + timeZone;
-  $("#latitude")[0].textContent = " " + latitdue;
-  $("#longitude")[0].textContent = " " +longitude;
-  $("#provider")[0].textContent = " " + provider;
-  $("#countryCode")[0].textContent = " " + countrycode;
-
+$.ajax({
+  url: `https://api.ipfind.com/?ip=${ip}&auth=96c8bea0-25b5-4ebd-a162-3d65cf6430e1`,
+  success: function(data){
+    console.log(data);
+  }
 });
+// $("#flag").attr("src", "https://api.ipfind.com/flag?ip="+ip+"&auth=96c8bea0-25b5-4ebd-a162-3d65cf6430e1");
+// $.getJSON('https://ipfind.com/?ip='+ip+'&auth=96c8bea0-25b5-4ebd-a162-3d65cf6430e1', function(data) {
+//   console.log(JSON.stringify(data, null, 2));
+//   info = data;
+//   var ip = info.ip_address;
+//   var country = info.country;
+//   var city = info.city;
+//   var timeZone = info.timezone;
+//   var latitdue = info.latitude;
+//   var longitude = info.longitude;
+//   var countrycode;
+
+//   //return info
+//   $("#ip")[0].textContent = " " + ip;
+//   $("#country")[0].textContent = " " + country;
+//   $("#city")[0].textContent = " " + city;
+//   $("#timezone")[0].textContent = " " + timeZone;
+//   $("#latitude")[0].textContent = " " + latitdue;
+//   $("#longitude")[0].textContent = " " +longitude;
+//   $("#provider")[0].textContent = " " + provider;
+//   $("#countryCode")[0].textContent = " " + countrycode;
+//   
+
+
+// });
 }
